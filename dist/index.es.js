225e4d8f06f4a082e3b99ae7bbff3e84380882cb
sky <854323752@qq.com>
2018-07-06 03:02:00 +0800

    修改为原型方案

--- dist/index.es.js
+++ dist/index.es.js
@@ -110,150 +110,141 @@ var canvasToBlob = createCommonjsModule(function (module) {
 })(window);
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
+/**
+ * @author sky
+ * @email 854323752@qq.com
+ * @create date 2018-06-25 05:00:10
+ * @modify date 2018-06-25 05:00:10
+ * @desc 图片质量优化（Image Quality Optimize）
+*/
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+/**
+ * 构造函数
+ * @param {Number} standard 基准值，表示是否需要进行缩放操作。默认：600
+ */
+function IQO(standard) {
+  this.prefix = '[IQO]';
+  this._URLCompat();
 
-var IQO = function () {
-  /**
-   * 构造函数
-   * @param {Number} standard 基准值，表示是否需要进行缩放操作。默认：600
-   */
-  function IQO(standard) {
-    _classCallCheck(this, IQO);
+  if (!isNaN(standard) && standard > 0) {
+    this.standard = standard;
+  } else {
+    this.standard = 600;
+  }
+}
 
-    this.prefix = '[IQO]';
-    this._URLCompat();
+var internal = IQO.prototype = {};
 
-    if (!isNaN(standard) && standard > 0) {
-      this.standard = standard;
+internal._URLCompat = function () {
+  if (window.URL) {
+    this.URL = window.URL;
+  } else if (window.webkitURL) {
+    this.URL = window.webkitURL;
+  } else if (window.mozURL) {
+    this.URL = window.mozURL;
+  } else if (window.msURL) {
+    this.URL = window.msURL;
+  } else {
+    throw new Error(this.prefix + '`window.URL` is not support! please update your browser.');
+  }
+};
+
+internal._file2Image = function (url) {
+  var _this = this;
+
+  return new Promise(function (resolve, reject) {
+    var $$image = void 0;
+    if (_this.image) {
+      $$image = _this.image;
     } else {
-      this.standard = 600;
+      $$image = _this.image = document.createElement('img');
+      $$image.style.display = 'none';
+      document.body.append($$image);
     }
+
+    $$image.onload = function () {
+      return resolve($$image);
+    };
+    $$image.onerror = function () {
+      return reject(new Error(_this.prefix + 'image loading failed!'));
+    };
+    $$image.src = url;
+
+    // 确保缓存的图片也能触发onload事件
+    if ($$image.complete || $$image.complete === 'undefined') {
+      $$image.src = 'data:image/jpeg;base64,clean' + new Date();
+      $$image.src = url;
+    }
+  });
+};
+
+internal._drawImage = function (image, type, quality, scale) {
+  var _this2 = this;
+
+  return new Promise(function (resolve, reject) {
+    // Optimize: 缩小体积以减小图片大小
+    if (image.width < _this2.standard && image.height < _this2.standard) {
+      scale = 1;
+    }
+
+    var $$canvas = void 0;
+    var ctx = void 0;
+    if (_this2.canvas) {
+      $$canvas = _this2.canvas;
+      ctx = _this2.ctx;
+    } else {
+      $$canvas = _this2.canvas = document.createElement('canvas');
+      ctx = _this2.ctx = $$canvas.getContext('2d');
+    }
+
+    var width = image.width * scale / 100;
+    var height = image.height * scale / 100;
+
+    $$canvas.width = width;
+    $$canvas.height = height;
+    try {
+      // 在canvas中绘制图片
+      ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height);
+      // 将图片转换成base64
+      // Note: quality属性只有jpg和webp格式才有效
+      $$canvas.toBlob(function (blob) {
+        // 清除画布
+        ctx.clearRect(0, 0, width, height);
+        resolve(blob);
+      }, type, quality / 100);
+    } catch (ex) {
+      ctx.clearRect(0, 0, width, height);
+      reject(ex);
+    }
+  });
+};
+
+IQO.prototype.compress = function (file, quality, scale) {
+  var _this3 = this;
+
+  var type = file.type || 'image/' + file.substr(file.lastIndexOf('.') + 1);
+
+  quality = Number(quality);
+  if (isNaN(quality) || quality < 0 || quality > 100) {
+    quality = 95;
   }
 
-  _createClass(IQO, [{
-    key: '_URLCompat',
-    value: function _URLCompat() {
-      if (window.URL) {
-        this.URL = window.URL;
-      } else if (window.webkitURL) {
-        this.URL = window.webkitURL;
-      } else if (window.mozURL) {
-        this.URL = window.mozURL;
-      } else if (window.msURL) {
-        this.URL = window.msURL;
-      } else {
-        throw new Error(this.prefix + '`window.URL` is not support! please update your browser.');
-      }
-    }
-  }, {
-    key: '_file2Image',
-    value: function _file2Image(url) {
-      var _this = this;
+  scale = Number(scale);
+  if (isNaN(scale) || scale < 0 || scale > 100) {
+    scale = 70;
+  }
 
-      return new Promise(function (resolve, reject) {
-        var $$image = void 0;
-        if (_this.image) {
-          $$image = _this.image;
-        } else {
-          $$image = _this.image = document.createElement('img');
-          $$image.style.display = 'none';
-          document.body.append($$image);
-        }
+  var url = this.URL.createObjectURL(file);
 
-        $$image.onload = function () {
-          return resolve($$image);
-        };
-        $$image.onerror = function () {
-          return reject(new Error(_this.prefix + 'image loading failed!'));
-        };
-        $$image.src = url;
-
-        // 确保缓存的图片也能触发onload事件
-        if ($$image.complete || $$image.complete === 'undefined') {
-          $$image.src = 'data:image/jpeg;base64,clean' + new Date();
-          $$image.src = url;
-        }
-      });
-    }
-
-    // 测试结果：在图片质量调至45、原图宽高在1000左右的情况下，图片大小下降近5倍
-
-  }, {
-    key: '_drawImage',
-    value: function _drawImage(image, type, quality, scale) {
-      var _this2 = this;
-
-      return new Promise(function (resolve, reject) {
-        // Optimize: 缩小体积以减小图片大小
-        if (image.width < _this2.standard && image.height < _this2.standard) {
-          scale = 1;
-        }
-
-        var $$canvas = void 0;
-        var ctx = void 0;
-        if (_this2.canvas) {
-          $$canvas = _this2.canvas;
-          ctx = _this2.ctx;
-        } else {
-          $$canvas = _this2.canvas = document.createElement('canvas');
-          ctx = _this2.ctx = $$canvas.getContext('2d');
-        }
-
-        var width = image.width * scale / 100;
-        var height = image.height * scale / 100;
-
-        $$canvas.width = width;
-        $$canvas.height = height;
-        try {
-          // 在canvas中绘制图片
-          ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height);
-          // 将图片转换成base64
-          // Note: quality属性只有jpg和webp格式才有效
-          $$canvas.toBlob(function (blob) {
-            // 清除画布
-            ctx.clearRect(0, 0, width, height);
-            resolve(blob);
-          }, type, quality / 100);
-        } catch (ex) {
-          ctx.clearRect(0, 0, width, height);
-          reject(ex);
-        }
-      });
-    }
-  }, {
-    key: 'compress',
-    value: function compress(file, quality, scale) {
-      var _this3 = this;
-
-      var type = file.type || 'image/' + file.substr(file.lastIndexOf('.') + 1);
-
-      quality = Number(quality);
-      if (isNaN(quality) || quality < 0 || quality > 100) {
-        quality = 95;
-      }
-
-      scale = Number(scale);
-      if (isNaN(scale) || scale < 0 || scale > 100) {
-        scale = 70;
-      }
-
-      var url = this.URL.createObjectURL(file);
-
-      return this._file2Image(url).then(function (image) {
-        return _this3._drawImage(image, type, quality, scale);
-      }).then(function (blob) {
-        _this3.URL.revokeObjectURL(url);
-        return blob.size < file.size ? blob : file;
-      }).catch(function (error) {
-        _this3.URL.revokeObjectURL(url);
-        throw error;
-      });
-    }
-  }]);
-
-  return IQO;
-}();
+  return this._file2Image(url).then(function (image) {
+    return _this3._drawImage(image, type, quality, scale);
+  }).then(function (blob) {
+    _this3.URL.revokeObjectURL(url);
+    return blob.size < file.size ? blob : file;
+  }).catch(function (error) {
+    _this3.URL.revokeObjectURL(url);
+    throw error;
+  });
+};
 
 export default IQO;
--- index.js
+++ index.js
@@ -7,13 +7,13 @@
 */
 
 import 'blueimp-canvas-to-blob'
-export default class IQO {
-  /**
-   * 构造函数
-   * @param {Number} standard 基准值，表示是否需要进行缩放操作。默认：600
-   */
-  constructor (standard) {
-    this.prefix = '[IQO]'
+
+/**
+ * 构造函数
+ * @param {Number} standard 基准值，表示是否需要进行缩放操作。默认：600
+ */
+function IQO (standard) {
+  this.prefix = '[IQO]'
     this._URLCompat()
 
     if (!isNaN(standard) && standard > 0) {
@@ -21,109 +21,111 @@ export default class IQO {
     } else {
       this.standard = 600
     }
-  }
+}
 
-  _URLCompat () {
-    if (window.URL) {
-      this.URL = window.URL
-    } else if (window.webkitURL) {
-      this.URL = window.webkitURL
-    } else if (window.mozURL) {
-      this.URL = window.mozURL
-    } else if (window.msURL) {
-      this.URL = window.msURL
-    } else {
-      throw new Error(this.prefix + '`window.URL` is not support! please update your browser.')
-    }
-  }
+var internal = IQO.prototype = {}
 
-  _file2Image (url) {
-    return new Promise((resolve, reject) => {
-      let $$image
-      if (this.image) {
-        $$image = this.image
-      } else {
-        $$image = this.image = document.createElement('img')
-        $$image.style.display = 'none'
-        document.body.append($$image)
-      }
-
-      $$image.onload = () => resolve($$image)
-      $$image.onerror = () => reject(new Error(this.prefix + 'image loading failed!'))
-      $$image.src = url
-
-      // 确保缓存的图片也能触发onload事件
-      if ($$image.complete || $$image.complete === 'undefined') {
-        $$image.src = 'data:image/jpeg;base64,clean' + new Date()
-        $$image.src = url
-      }
-    })
-  }
-
-  // 测试结果：在图片质量调至45、原图宽高在1000左右的情况下，图片大小下降近5倍
-  _drawImage (image, type, quality, scale) {
-    return new Promise((resolve, reject) => {
-      // Optimize: 缩小体积以减小图片大小
-      if (image.width < this.standard && image.height < this.standard) {
-        scale = 1
-      }
-
-      let $$canvas
-      let ctx
-      if (this.canvas) {
-        $$canvas = this.canvas
-        ctx = this.ctx
-      } else {
-        $$canvas = this.canvas = document.createElement('canvas')
-        ctx = this.ctx = $$canvas.getContext('2d')
-      }
-
-      let width = image.width * scale / 100
-      let height = image.height * scale / 100
-
-      $$canvas.width = width
-      $$canvas.height = height
-      try {
-        // 在canvas中绘制图片
-        ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height)
-        // 将图片转换成base64
-        // Note: quality属性只有jpg和webp格式才有效
-        $$canvas.toBlob(blob => {
-          // 清除画布
-          ctx.clearRect(0, 0, width, height)
-          resolve(blob)
-        }, type, quality / 100)
-      } catch (ex) {
-        ctx.clearRect(0, 0, width, height)
-        reject(ex)
-      }
-    })
-  }
-
-  compress (file, quality, scale) {
-    let type = file.type || 'image/' + file.substr(file.lastIndexOf('.') + 1)
-
-    quality = Number(quality)
-    if (isNaN(quality) || quality < 0 || quality > 100) {
-      quality = 95
-    }
-
-    scale = Number(scale)
-    if (isNaN(scale) || scale < 0 || scale > 100) {
-      scale = 70
-    }
-
-    let url = this.URL.createObjectURL(file)
-
-    return this._file2Image(url)
-      .then(image => this._drawImage(image, type, quality, scale))
-      .then(blob => {
-        this.URL.revokeObjectURL(url)
-        return blob.size < file.size ? blob : file
-      })
-      .catch(error => {
-        this.URL.revokeObjectURL(url)
-        throw error
-      })
+internal._URLCompat = function () {
+  if (window.URL) {
+    this.URL = window.URL
+  } else if (window.webkitURL) {
+    this.URL = window.webkitURL
+  } else if (window.mozURL) {
+    this.URL = window.mozURL
+  } else if (window.msURL) {
+    this.URL = window.msURL
+  } else {
+    throw new Error(this.prefix + '`window.URL` is not support! please update your browser.')
   }
 }
+
+internal._file2Image = function (url) {
+  return new Promise((resolve, reject) => {
+    let $$image
+    if (this.image) {
+      $$image = this.image
+    } else {
+      $$image = this.image = document.createElement('img')
+      $$image.style.display = 'none'
+      document.body.append($$image)
+    }
+
+    $$image.onload = () => resolve($$image)
+    $$image.onerror = () => reject(new Error(this.prefix + 'image loading failed!'))
+    $$image.src = url
+
+    // 确保缓存的图片也能触发onload事件
+    if ($$image.complete || $$image.complete === 'undefined') {
+      $$image.src = 'data:image/jpeg;base64,clean' + new Date()
+      $$image.src = url
+    }
+  })
+}
+
+internal._drawImage = function (image, type, quality, scale) {
+  return new Promise((resolve, reject) => {
+    // Optimize: 缩小体积以减小图片大小
+    if (image.width < this.standard && image.height < this.standard) {
+      scale = 1
+    }
+
+    let $$canvas
+    let ctx
+    if (this.canvas) {
+      $$canvas = this.canvas
+      ctx = this.ctx
+    } else {
+      $$canvas = this.canvas = document.createElement('canvas')
+      ctx = this.ctx = $$canvas.getContext('2d')
+    }
+
+    let width = image.width * scale / 100
+    let height = image.height * scale / 100
+
+    $$canvas.width = width
+    $$canvas.height = height
+    try {
+      // 在canvas中绘制图片
+      ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height)
+      // 将图片转换成base64
+      // Note: quality属性只有jpg和webp格式才有效
+      $$canvas.toBlob(blob => {
+        // 清除画布
+        ctx.clearRect(0, 0, width, height)
+        resolve(blob)
+      }, type, quality / 100)
+    } catch (ex) {
+      ctx.clearRect(0, 0, width, height)
+      reject(ex)
+    }
+  })
+}
+
+IQO.prototype.compress = function (file, quality, scale) {
+  let type = file.type || 'image/' + file.substr(file.lastIndexOf('.') + 1)
+
+  quality = Number(quality)
+  if (isNaN(quality) || quality < 0 || quality > 100) {
+    quality = 95
+  }
+
+  scale = Number(scale)
+  if (isNaN(scale) || scale < 0 || scale > 100) {
+    scale = 70
+  }
+
+  let url = this.URL.createObjectURL(file)
+
+  return this._file2Image(url)
+    .then(image => this._drawImage(image, type, quality, scale))
+    .then(blob => {
+      this.URL.revokeObjectURL(url)
+      return blob.size < file.size ? blob : file
+    })
+    .catch(error => {
+      this.URL.revokeObjectURL(url)
+      throw error
+    })
+}
+
+export default IQO
