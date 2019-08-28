a09580d50d07836a959fbc47aa793f5c0adb3fed
sky <854323752@qq.com>
2019-08-28 15:21:11 +0800

    优化代码

--- dist/iqo.es.js
+++ dist/iqo.es.js
@@ -3,13 +3,13 @@ function createCommonjsModule(fn, module) {
 }
 
 var canvasToBlob = createCommonjsModule(function (module) {
-(function (window) {
+(function(window) {
 
   var CanvasPrototype =
     window.HTMLCanvasElement && window.HTMLCanvasElement.prototype;
   var hasBlobConstructor =
     window.Blob &&
-    (function () {
+    (function() {
       try {
         return Boolean(new Blob())
       } catch (e) {
@@ -19,7 +19,7 @@ var canvasToBlob = createCommonjsModule(function (module) {
   var hasArrayBufferViewSupport =
     hasBlobConstructor &&
     window.Uint8Array &&
-    (function () {
+    (function() {
       try {
         return new Blob([new Uint8Array(100)]).size === 100
       } catch (e) {
@@ -37,7 +37,7 @@ var canvasToBlob = createCommonjsModule(function (module) {
     window.atob &&
     window.ArrayBuffer &&
     window.Uint8Array &&
-    function (dataURI) {
+    function(dataURI) {
       var matches,
         mediaType,
         isBase64,
@@ -83,9 +83,9 @@ var canvasToBlob = createCommonjsModule(function (module) {
     };
   if (window.HTMLCanvasElement && !CanvasPrototype.toBlob) {
     if (CanvasPrototype.mozGetAsFile) {
-      CanvasPrototype.toBlob = function (callback, type, quality) {
+      CanvasPrototype.toBlob = function(callback, type, quality) {
         var self = this;
-        setTimeout(function () {
+        setTimeout(function() {
           if (quality && CanvasPrototype.toDataURL && dataURLtoBlob) {
             callback(dataURLtoBlob(self.toDataURL(type, quality)));
           } else {
@@ -94,15 +94,15 @@ var canvasToBlob = createCommonjsModule(function (module) {
         });
       };
     } else if (CanvasPrototype.toDataURL && dataURLtoBlob) {
-      CanvasPrototype.toBlob = function (callback, type, quality) {
+      CanvasPrototype.toBlob = function(callback, type, quality) {
         var self = this;
-        setTimeout(function () {
+        setTimeout(function() {
           callback(dataURLtoBlob(self.toDataURL(type, quality)));
         });
       };
     }
   }
-  if (module.exports) {
+  if ( module.exports) {
     module.exports = dataURLtoBlob;
   } else {
     window.dataURLtoBlob = dataURLtoBlob;
@@ -116,132 +116,114 @@ var canvasToBlob = createCommonjsModule(function (module) {
  * @create date 2018-06-25 05:00:10
  * @modify date 2018-06-25 05:00:10
  * @desc 图片质量优化（Image Quality Optimize）
-*/
-
+ */
 /**
  * 构造函数
  * @param {Number} standard 基准值，表示是否需要进行图片缩放操作。默认：600
  */
+
 function IQO(standard) {
   this.prefix = '[IQO]';
   this.standard = !isNaN(standard) && standard > 0 ? standard : 600;
-  this._URLCompat();
+  this.URL = this._URLCompat();
 }
 
 var internal = IQO.prototype = {
   constructor: IQO
 };
+/**
+ * 兼容的url生成器
+ */
 
 internal._URLCompat = function () {
-  if ('URL' in window) {
-    this.URL = window.URL;
+  if ('URL' in window && typeof window.URL.createObjectURL === 'function') {
+    return window.URL;
   } else if ('webkitURL' in window) {
-    this.URL = window.webkitURL;
+    return window.webkitURL;
   } else if ('mozURL' in window) {
-    this.URL = window.mozURL;
+    return window.mozURL;
   } else if ('msURL' in window) {
-    this.URL = window.msURL;
-  } else {
-    this.URL = null;
+    return window.msURL;
   }
+
+  return null;
 };
+/**
+ * 通过window.URL生成文件url
+ * @param {file} file
+ */
+
 
 internal._generateFileURLByURL = function (file) {
-  // console.log('使用window.URL生成URL')
   return this.URL.createObjectURL(file);
 };
+/**
+ * 通过FileReader类生成文件url
+ */
+
 
 internal._generateFileURLByFileReader = function (file, success, fail) {
-  // console.log('使用FileReader生成URL')
   var fileReader = new FileReader();
 
   fileReader.onload = function () {
     success(fileReader.result);
   };
 
-  fileReader.onerror = function (error) {
-    // console.log('FileReader读取文件失败')
-    fail(error);
-  };
-
+  fileReader.onerror = fail;
   fileReader.readAsDataURL(file);
 };
-
 /**
- * 文件类型转换为url
+ * 生成文件url
  * @param {file}} file
  */
+
+
 internal._generateFileURL = function (file) {
   var _this = this;
 
   return new Promise(function (resolve, reject) {
-    // console.log('将文件类型转换成URL')
     if (_this.URL) {
       resolve(_this._generateFileURLByURL(file));
     } else if ('FileReader' in window) {
-      _this._generateFileURLByFileReader(file, function (url) {
-        resolve(url);
-      }, function (error) {
-        reject(error);
-      });
+      _this._generateFileURLByFileReader(file, resolve, reject);
     } else {
-      // console.log('您的浏览器不支持window.URL和FileReader！')
       reject(new Error('您的浏览器不支持window.URL和FileReader！'));
     }
   });
 };
+/**
+ * 注销文件url
+ * @param {string} url
+ */
+
 
 internal._revokeFileURL = function (url) {
   this.URL && this.URL.revokeObjectURL(url);
 };
-
 /**
  * 文件类型转换为图片类型
  * @param {base64} url
  */
+
+
 internal._file2Image = function (url) {
   var _this2 = this;
 
   return new Promise(function (resolve, reject) {
-    console.log('加载图片');
     var image = new Image();
 
     image.onload = function () {
       return resolve(image);
     };
+
     image.onerror = function (error) {
-      console.log(_this2.prefix + '图片加载失败！', error);
+      console.error(_this2.prefix + '图片加载失败！', error);
       reject(error);
     };
 
     image.src = url;
   });
 };
-
-/**
- * 获取到canvas每一个像素，然后检查这张图片是否是黑色的图片
- * 注：不对黑色的图片进行压缩
- * @param {array} imageData 
- */
-// internal._checkImageData = function (imageData) {
-//   let len = imageData ? imageData.length : 0
-//   let otherData = 0
-
-//   if (len) {
-//     for (let i = 0; i < len; i += 4) {
-//       let r = imageData[i]
-//       let g = imageData[i + 1]
-//       let b = imageData[i + 2]
-
-//       if (r !== 0 && g !== 0 && b !== 0) {
-//         otherData++
-//       }
-//     }
-//   }
-
-//   return otherData !== 0
-// }
-
 /**
  * 图片绘制，压缩
  * @param {image} image
@@ -249,14 +231,15 @@ internal._file2Image = function (url) {
  * @param {number} quality
  * @param {number} scale
  */
+
+
 internal._drawImage = function (image, type, quality, scale) {
   var _this3 = this;
 
   return new Promise(function (resolve, reject) {
-    // console.log('开始画图')
     // OPTIMIZE: 缩小体积以减小图片大小
-    scale = image.width < _this3.standard && image.height < _this3.standard ? 1 : scale / 100;
-    // OPTIMIZE: 减少质量以减小图片大小
+    scale = image.width < _this3.standard && image.height < _this3.standard ? 1 : scale / 100; // OPTIMIZE: 减少质量以减小图片大小
+
     quality = quality / 100;
 
     if (!_this3.canvas) {
@@ -266,95 +249,81 @@ internal._drawImage = function (image, type, quality, scale) {
 
     var $$canvas = _this3.canvas;
     var ctx = _this3.ctx;
-
     var width = image.width * scale;
-    var height = image.height * scale;
+    var height = image.height * scale; // 设置canvas的尺寸
 
     $$canvas.setAttribute('width', width);
     $$canvas.setAttribute('height', height);
     $$canvas.width = width;
     $$canvas.height = height;
 
-    // console.log('是否支持toBlob：' + (typeof $$canvas.toBlob === 'function'))
     try {
-      // 完成blob对象的回调函数
-      var done = function done(blob) {
-        return resolve(blob);
-      };
       // 1. 清除画布
       ctx.clearRect(0, 0, width, height);
-      ctx.save();
-      // 2. 在canvas中绘制图片
+      ctx.save(); // 2. 在canvas中绘制图片
+
       ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height);
-      ctx.restore();
-      // let imageData = ctx.getImageData(0, 0, width, height)
-      // if (!this._checkImageData(imageData)) {
-      //   reject(new Error('不对全黑图片进行压缩操作！'))
-      //   return
-      // }
-      // 4. 将图片转换成base64
+      ctx.restore(); // 4. 将图片转换成base64
       // NOTE: quality属性只有jpg和webp格式才有效
+
       if ($$canvas.toBlob) {
-        $$canvas.toBlob(done, type, quality);
+        $$canvas.toBlob(resolve, type, quality);
       } else {
-        done(canvasToBlob($$canvas.toDataURL(type, quality)));
+        resolve(canvasToBlob($$canvas.toDataURL(type, quality)));
       }
     } catch (ex) {
       reject(ex);
     }
   });
 };
-
 /**
  * export 压缩
  * @param {file} file
  * @param {number} quality
  * @param {number} scale
  */
+
+
 IQO.prototype.compress = function (file, quality, scale) {
   var _this4 = this;
 
   var type = file.type || 'image/' + file.substr(file.lastIndexOf('.') + 1);
-  var url1 = null;
-
+  var generatedUrl = null;
   quality = Number(quality);
-  if (isNaN(quality) || quality < 0 || quality > 100) {
-    quality = 95;
+
+  if (isNaN(quality) || quality <= 0 || quality > 100) {
+    quality = 70;
   }
 
   scale = Number(scale);
-  if (isNaN(scale) || scale < 0 || scale > 100) {
+
+  if (isNaN(scale) || scale <= 0 || scale > 100) {
     scale = 70;
   }
 
   return this._generateFileURL(file).then(function (url) {
-    return _this4._file2Image(url1 = url);
+    return _this4._file2Image(generatedUrl = url);
   }).then(function (image) {
     return _this4._drawImage(image, type, quality, scale);
   }).then(function (blob) {
-    var result = null;
-
-    // 释放url的内存
-    _this4._revokeFileURL(url1);
     // 适配结果图片
     if (blob && blob.size > 0 && blob.size < file.size) {
       var date = new Date();
       blob.lastModified = date.getTime();
       blob.lastModifiedDate = date;
       blob.name = file.name;
-      result = blob;
-    } else {
-      result = file;
+      return blob;
     }
-    // console.log(Object.prototype.toString.call(result))
 
-    return result;
+    return file;
   }).catch(function (error) {
-    // 释放url的内存
-    _this4._revokeFileURL(url1);
-    // console.log('文件压缩失败！', error)
     console.error(error);
     return file;
+  }).then(function (finalFile) {
+    // 释放url的内存
+    _this4._revokeFileURL(generatedUrl);
+
+    return finalFile;
   });
 };
 
--- index.js
+++ index.js
@@ -4,82 +4,85 @@
  * @create date 2018-06-25 05:00:10
  * @modify date 2018-06-25 05:00:10
  * @desc 图片质量优化（Image Quality Optimize）
-*/
+ */
 
-import toBlob from 'blueimp-canvas-to-blob'
+import dataURLtoBlob from 'blueimp-canvas-to-blob'
 
 /**
  * 构造函数
  * @param {Number} standard 基准值，表示是否需要进行图片缩放操作。默认：600
  */
-function IQO (standard) {
+function IQO(standard) {
   this.prefix = '[IQO]'
   this.standard = !isNaN(standard) && standard > 0 ? standard : 600
-  this._URLCompat()
+  this.URL = this._URLCompat()
 }
 
-var internal = IQO.prototype = {
+const internal = (IQO.prototype = {
   constructor: IQO
-}
+})
 
-internal._URLCompat = function () {
-  if ('URL' in window) {
-    this.URL = window.URL
+/**
+ * 兼容的url生成器
+ */
+internal._URLCompat = function() {
+  if ('URL' in window && typeof window.URL.createObjectURL === 'function') {
+    return window.URL
   } else if ('webkitURL' in window) {
-    this.URL = window.webkitURL
+    return window.webkitURL
   } else if ('mozURL' in window) {
-    this.URL = window.mozURL
+    return window.mozURL
   } else if ('msURL' in window) {
-    this.URL = window.msURL
-  } else {
-    this.URL = null
+    return window.msURL
   }
+
+  return null
 }
 
-internal._generateFileURLByURL = function (file) {
-  // console.log('使用window.URL生成URL')
+/**
+ * 通过window.URL生成文件url
+ * @param {file} file
+ */
+internal._generateFileURLByURL = function(file) {
   return this.URL.createObjectURL(file)
 }
 
-internal._generateFileURLByFileReader = function (file, success, fail) {
-  // console.log('使用FileReader生成URL')
+/**
+ * 通过FileReader类生成文件url
+ */
+internal._generateFileURLByFileReader = function(file, success, fail) {
   let fileReader = new FileReader()
 
-  fileReader.onload = function () {
+  fileReader.onload = function() {
     success(fileReader.result)
   }
 
-  fileReader.onerror = function (error) {
-    // console.log('FileReader读取文件失败')
-    fail(error)
-  }
+  fileReader.onerror = fail
 
   fileReader.readAsDataURL(file)
 }
 
 /**
- * 文件类型转换为url
+ * 生成文件url
  * @param {file}} file
  */
-internal._generateFileURL = function (file) {
+internal._generateFileURL = function(file) {
   return new Promise((resolve, reject) => {
-    // console.log('将文件类型转换成URL')
     if (this.URL) {
       resolve(this._generateFileURLByURL(file))
     } else if ('FileReader' in window) {
-      this._generateFileURLByFileReader(file, function (url) {
-        resolve(url)
-      }, function (error) {
-        reject(error)
-      })
+      this._generateFileURLByFileReader(file, resolve, reject)
     } else {
-      // console.log('您的浏览器不支持window.URL和FileReader！')
       reject(new Error('您的浏览器不支持window.URL和FileReader！'))
     }
   })
 }
 
-internal._revokeFileURL = function (url) {
+/**
+ * 注销文件url
+ * @param {string} url
+ */
+internal._revokeFileURL = function(url) {
   this.URL && this.URL.revokeObjectURL(url)
 }
 
@@ -87,14 +90,13 @@ internal._revokeFileURL = function (url) {
  * 文件类型转换为图片类型
  * @param {base64} url
  */
-internal._file2Image = function (url) {
+internal._file2Image = function(url) {
   return new Promise((resolve, reject) => {
-    console.log('加载图片')
     let image = new Image()
 
     image.onload = () => resolve(image)
-    image.onerror = (error) => {
-      console.log(this.prefix + '图片加载失败！', error)
+    image.onerror = error => {
+      console.error(this.prefix + '图片加载失败！', error)
       reject(error)
     }
 
@@ -103,41 +105,19 @@ internal._file2Image = function (url) {
 }
 
 /**
- * 获取到canvas每一个像素，然后检查这张图片是否是黑色的图片
- * 注：不对黑色的图片进行压缩
- * @param {array} imageData 
- */
-// internal._checkImageData = function (imageData) {
-//   let len = imageData ? imageData.length : 0
-//   let otherData = 0
-
-//   if (len) {
-//     for (let i = 0; i < len; i += 4) {
-//       let r = imageData[i]
-//       let g = imageData[i + 1]
-//       let b = imageData[i + 2]
-
-//       if (r !== 0 && g !== 0 && b !== 0) {
-//         otherData++
-//       }
-//     }
-//   }
-
-//   return otherData !== 0
-// }
-
-/**
  * 图片绘制，压缩
  * @param {image} image
  * @param {string} type
  * @param {number} quality
  * @param {number} scale
  */
-internal._drawImage = function (image, type, quality, scale) {
+internal._drawImage = function(image, type, quality, scale) {
   return new Promise((resolve, reject) => {
-    // console.log('开始画图')
     // OPTIMIZE: 缩小体积以减小图片大小
-    scale = image.width < this.standard && image.height < this.standard ? 1 : scale / 100
+    scale =
+      image.width < this.standard && image.height < this.standard
+        ? 1
+        : scale / 100
     // OPTIMIZE: 减少质量以减小图片大小
     quality = quality / 100
 
@@ -152,32 +132,25 @@ internal._drawImage = function (image, type, quality, scale) {
     let width = image.width * scale
     let height = image.height * scale
 
+    // 设置canvas的尺寸
     $$canvas.setAttribute('width', width)
     $$canvas.setAttribute('height', height)
     $$canvas.width = width
     $$canvas.height = height
 
-    // console.log('是否支持toBlob：' + (typeof $$canvas.toBlob === 'function'))
     try {
-      // 完成blob对象的回调函数
-      let done = (blob) => resolve(blob)
       // 1. 清除画布
       ctx.clearRect(0, 0, width, height)
       ctx.save()
       // 2. 在canvas中绘制图片
       ctx.drawImage(image, 0, 0, image.width, image.height, 0, 0, width, height)
       ctx.restore()
-      // let imageData = ctx.getImageData(0, 0, width, height)
-      // if (!this._checkImageData(imageData)) {
-      //   reject(new Error('不对全黑图片进行压缩操作！'))
-      //   return
-      // }
       // 4. 将图片转换成base64
       // NOTE: quality属性只有jpg和webp格式才有效
       if ($$canvas.toBlob) {
-        $$canvas.toBlob(done, type, quality)
+        $$canvas.toBlob(resolve, type, quality)
       } else {
-        done(toBlob($$canvas.toDataURL(type, quality)))
+        resolve(dataURLtoBlob($$canvas.toDataURL(type, quality)))
       }
     } catch (ex) {
       reject(ex)
@@ -191,48 +164,43 @@ internal._drawImage = function (image, type, quality, scale) {
  * @param {number} quality
  * @param {number} scale
  */
-IQO.prototype.compress = function (file, quality, scale) {
+IQO.prototype.compress = function(file, quality, scale) {
   let type = file.type || 'image/' + file.substr(file.lastIndexOf('.') + 1)
-  let url1 = null
+  let generatedUrl = null
 
   quality = Number(quality)
-  if (isNaN(quality) || quality < 0 || quality > 100) {
-    quality = 95
+  if (isNaN(quality) || quality <= 0 || quality > 100) {
+    quality = 70
   }
 
   scale = Number(scale)
-  if (isNaN(scale) || scale < 0 || scale > 100) {
+  if (isNaN(scale) || scale <= 0 || scale > 100) {
     scale = 70
   }
 
   return this._generateFileURL(file)
-    .then(url => this._file2Image((url1 = url)))
+    .then(url => this._file2Image((generatedUrl = url)))
     .then(image => this._drawImage(image, type, quality, scale))
     .then(blob => {
-      let result = null
-
-      // 释放url的内存
-      this._revokeFileURL(url1)
       // 适配结果图片
       if (blob && blob.size > 0 && blob.size < file.size) {
-        let date = new Date()
+        const date = new Date()
         blob.lastModified = date.getTime()
         blob.lastModifiedDate = date
         blob.name = file.name
-        result = blob
-      } else {
-        result = file
+        return blob
       }
-      // console.log(Object.prototype.toString.call(result))
 
-      return result
+      return file
     })
     .catch(error => {
-      // 释放url的内存
-      this._revokeFileURL(url1)
-      // console.log('文件压缩失败！', error)
       console.error(error)
       return file
+    })
+    .then(finalFile => {
+      // 释放url的内存
+      this._revokeFileURL(generatedUrl)
+      return finalFile
     })
 }
 
